9436c92fcb3b69a1cb2eac1a582bb34f42aaadf1
notoncebut2x <rpnealon@gmail.com>
2021-01-18 23:34:00 -0500

    added new images and added two new chapters

--- config.js
+++ config.js
@@ -38,11 +38,11 @@ var config = {
             audio: './audio/DCOHC_Meiwah_Slide_05.mp3',
             description: '<span class="quote">In Vietnam, we jokingly said, if you hear an explosion, that means you\'re still alive.</span><br><br>Larry recalls growing up in a war zone as a teenager and seeing dead bodies on the street on his way back from school. This repetitive and traumatic exposure to everyday violence numbed him to the daily atrocities he encountered. He had no other choice but to adopt a mental toughness that stayed with him into adulthood.',
             transcript: '<i>Actually I was born and grew up in a war zone. It\'s just the time I was born and the time that the communists up North took over the whole South Vietnam in 1975. April 30th of 75. That\'s the fall of Saigon. Still war going on a little bit, but that\'s part of the end of the war. And since then, Vietnam had been peace for the last 40 some years. They have some war right after that, probably 1976, 77 to Cambodia. And then with China in 1978. But that\'s it. After that, Vietnam have not had war for a long time. But few in Vietnam, like my age or they go today, we all born and grew up in a war zone. We see people, their body on the streets, coming back from school. You just come and walk by and take a look. If it\'s somebody that you know, you don\'t, you just walk on. Gun fire almost everywhere. If you look in the TV nowadays in some of the Middle East country, it\'s pretty bad at that time. And in Vietnam we always jokingly saying, but that\'s real is that, \‘if you hear an explosion, that means you\'re still alive.’ That\'s how bad. You can walk by somewhere, they have the explosion. Because it\'s a civil war. The Vietnamese communists, they\'re called Viet Cong. They everywhere. Especially when I was old enough at that time, in 1968 when they have the Tet Offensive, there\'s really fighting right inside the city. We probably numb with the war, so that\'s why we not die, scary, because it\'s everywhere is war anyway. So I still remember even teenager, I still very young, 12, 13. I still standing the roof of the building, not the red roof, the building too. We see the helicopter, military helicopter shoot out from both sides of that. And you can see the explosion from the target. And we\'re standing there watch, look like we\'re watching movie but it\'s real. So that give you a sense that when people go to war zone, their mentality is different than people who never see war before. That\'s not so obvious.</i>',
-            // chapterType: 'wide',
+            chapterType: 'wide',
             location: {
-                center: [106.3397879, 9.9385840],
-                zoom: 6,
-                pitch: 60,
+		    center: [105.00, 15.00],
+                zoom: 5.5,
+                pitch: 35,
                 bearing: 0.00
             },
             onChapterEnter: [],
@@ -58,7 +58,7 @@ var config = {
             location: {
                 center: [104.23839, 5.85966],
                 zoom: 4.8,
-                pitch: 45.00,
+                pitch: 50.00,
                 bearing: -0.02
             },
             onChapterEnter: [
@@ -205,10 +205,8 @@ var config = {
         },
         {
             id: 'cloc2',
-            //title: 'City Lights of China turned into Refugee Meeting Room',
-            image: './images/Slide_12_01.jpg',
+            image: './images/Slide_12.jpg',
             audio: './audio/DCOHC_Meiwah_Slide_12.mp3',
-            // description: 'After twelve years since Larry immigrated to America, he was reconnected with Rene Van Rooyen, the United Nations High Commissioner for Refugee, whom he had met at the refugee camp in Malaysia. <br><br>Since their reunion, Larry has reimagined his restaurant to not only feed diners, but to also serve as a meeting space for refugee communities and other organizations working to support them.',
             transcript:'I was in City Lights for 10 years, and I was in the basement for everyday. I used to work seven days, or six days, or six and a half days easily. And I\'m in the basement so long, and the ceiling was so low. I can jump up and can touch the ceiling. So, when I go out at lunchtime, after lunchtime to do errand, to go bank and do everything, so now I feel like the sky is that low that I could touch the sky, I mean just the feeling. It\'s kind of scary and I feel like, wow, I better not stay in the basement for another 10 years. I don\'t know what I\'d feel about psychologically. So, I want to get out, and I want to sell the restaurant to other people so we can open a new restaurant. So that\'s why, after I sold it in 1998, I thought that I could find another career, find another business, but I still miss the customers, and also the staff too, and the customers. So, a year later, some of the people, they still stay at City Lights after we sold, the people in the kitchen, they call me and say, ‘Hey, let\'s open another restaurant!’ I said, ‘well, I wanted to,’ because I myself, that\'s what they say, how much golf you can play everyday. So, I said, ‘well, I need to get back to the restaurant business.’ And I\'m probably one of the few people that get out of the restaurant business, still want to get in, because I love it. You have to like it.',
             location: {
                 center: [-77.04528, 38.91353],
@@ -229,7 +227,29 @@ var config = {
                 }
             ]
         },
-
+	{
+		id:'clocMenu',
+		image:'./images/Slide_13.jpg',
+		description:'Menu and Matchbook from City Lights of China',
+		location: {
+		    center:[-77.04528, 38.91353],
+                zoom: 19,
+                pitch: 45,
+                bearing: 60
+            },
+            onChapterEnter: [
+                {
+                    layer: 'restaurantLocations',
+                    opacity: 0.85
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'restaurantLocations',
+                    opacity: 0
+                }
+            ]
+	},
         {
             id: 'deadEndWestEnd ',
             title: '“Dead End” to West End',
@@ -284,8 +304,8 @@ var config = {
         {
             id: 'gtc',
             title: 'General Tso\’s Chicken: An American Chinese Dish',
-            image: './images/Slide_18_GeneralTso.jpg',
-            // audio: './audio/DCOHC_Meiwah_Slide_14.mp3',
+            image: './images/Slide_17_GeneralTso.jpg',
+            audio: './audio/DCOHC_Meiwah_Slide_17.mp3',
             description: '<span class="quote">In the United States, it doesn\'t matter how small a Chinese restaurant is or how big it is, you have to have General Tso\'s chicken. That\'s one of the main thing....You can\'t find it in China. Like the fortune cookie, you don\'t have in China... But people love it.</span>',
             transcript:'The Blair House is right across from the White House. The Blair House official name is the guest house of the president. The president of United States, like the Queen of England, the president of China, president of Korea, whatever come in, they don\'t stay in the hotel. The president invite them to stay in the Blair House. They have 14 bedrooms, very nice, beautiful there. So they have a lot of function that run by the State Department. So, the protocol office or the state department, so I\'ve been there many time for that. And one time, they asked us to cook, they called taste of China. So they have Ambassador from China here, and then the state department ... We cooked, and Ambassador said, "What did you cook today?" I said, "General Tso\'s chicken." And he looked at me and said, "What\'s that?" I said, even I speak in Chinese, he didn\'t know. He said, "What is General Tso’s?" I said, well, I said, "Ambassador, in this country, in the United States, it doesn\'t matter how small a Chinese restaurant is or how big it is, you have to have General Tso\'s chicken. That\'s one of the main thing." And it is not in China. You can\'t find in China. Like the fortune cookie, you don\'t have in China. So that\'s what we ... But people love it. So, the taste of people, going to the general public, that\'s more important. And of course, some people, they want something more fancy, you know? Beijing Duck is one of them. But in general, people like General Tso\'s chicken. So that ambassador asked me, "What\'s Meiwah mean?" I said, "Okay, Mei mean America, Wah mean China, American-Chinese." I said, "These two countries." I said that probably about six, seven years ago, I said, "Even these two countries so important to each other in the world that we cannot afford any kind of war, even economic war." Right now we have that. I said, "When we come disagreement, just the Chinese delegation and the American just come to Meiwah and have a meal, have a dinner, and drink Qingdao beer and solve the problem." And the ambassador said, "That\'s a good idea!',
             location: {
@@ -310,7 +330,7 @@ var config = {
         {
             id: 'meiwahChecyChase',
             title: 'Meiwah in Chevy Chase',
-            image: './images/Slide_16.JPG',
+            image: './images/Slide_18.JPG',
             audio: './audio/DCOHC_Meiwah_Slide_18.mp3',
             description: 'A few years after opening first Meiwah in West End, Larry opened his second Meiwah restaurant in Chevy Chase.',
             transcript:'We opened 2003. We opened DC in 2000, and then 2003, actually 2002, the owner of that building want Meiwah to open, and again, I went up there, and we are second floor, it\'s very hard to do. A lot of people in this industry, not just Chinese restaurant, that I know, they say, ‘Don\'t open restaurant on second floor. It\'s hard.’ But I said, ‘I opened in the basement, I\'m from the street level, why not second floor? Try it!’ [...] We\'re lucky. At that time, we got there, that\'s way before you all know about. Right across from us now is a high rise... I mean, a very big building, apartment building, and a garden, and the pub, and everything. Before, there was a parking lot for a depressed retail store called Hecht\'s. I don\'t know, you\'re probably too young to know Hecht\'s. Hecht\'s been gone for probably 16, 17 years ago. Yeah, they usually, Hecht\'s usually big like Macy\'s or something like that. But they got that across from us. It was usually just an empty lot, all parking for Hecht\'s company. So, when I got that space, it was a little bit scary too. First, we\'re on the second floor. Secondly, that area is not like it is today. Right now you go on Wisconsin [Avenue] near where our restaurant is, they have a lot of department store, a lot of big building. Those building, like Microsoft building across from us, also Whole Foods on there, all these, they\'re new, just only in the last probably five to 10 years.',
@@ -336,11 +356,37 @@ var config = {
         {
             id: 'meiwahChecyChase2',
             // title: 'Meiwah in Chevy Chase',
-            image: './images/Slide_17_01.jpg',
+            image: './images/Slide_19.jpg',
             audio: './audio/DCOHC_Meiwah_Slide_19.mp3',
             // description: 'A few years after opening first Meiwah in West End, Larry opened his second Meiwah restaurant in Chevy Chase.',
             transcript:'Even though Chevy Chase is only one block from the western area, that\'s the border of DC and Maryland, but the customers different. They mentally different. The people like suburb already, but the DC is more of a tourist and more of the people younger generation and people that lived there for long. So it\'s two different kinds of town. But again, they have people that work in DC. They eat lunch in DC at Meiwah and then at night they live in Chevy Chase but they start there and then they come to us for dinner. So we have both. That\'s I think that\'s the reason we can open the restaurant on second floor and still survive because we already have the client there. First they didn\'t know us. And quite frankly a lot of them, from my old restaurant still [inaudible] that\'s why I told you, I can see if you have pregnant, the son and now the son born, grew up, and now go the college and get married, and other see many generations because 30 years, a long time for many generation. You can see four generation come in together to eat. But this what is that you can build up a clientele that they feel very loyal to you. I have so many of this customer they\'ve been with me unless they move somewhere or some of them pass away but they still come to us. So we have a good time. And so that\'s why important is you have to repeat customer in this business. You can just say, okay, you come you come you don\'t say a one shot No. You have to take care of the customer today, they feel good when they come in they feel better when they leave',
             location: {
+                center: [-77.08843, 38.96207],
+                zoom: 17.6,
+                pitch: 60,
+                bearing: -10.00
+            },
+            onChapterEnter: [
+                {
+                    layer: 'meiwah_CC',
+                    opacity: .85
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'meiwah_CC',
+                    opacity: 0
+                }
+            ]
+        },
+ 	{
+            id: 'epilogue',
+            title: 'What does Washington, DC mean to you?',
+            image: './images/Slide_20.jpg',
+            audio: './audio/DCOHC_Meiwah_Slide_19.mp3',
+            description: '<span class="quote">I lived in DC since 1988, so it\'s 31 years. So this is a place that I call home, because even in Vietnam, I left Vietnam when I was 20, so only 20 years, and then Tennessee, and then, so here is the place that I lived the longest […] the greater Washington DC, it\'s very dynamic, and trust me, it\'s like the melting pot...In this area, you speak with an accent, nobody pays attention to you. But in some other states, the people say, ‘who is this? Where\'s he come from?’ But here, it\'s okay. </span>',
+            transcript: 'I lived in DC since 1988, so it\'s 31 years. So this is a place that I call home, because even in Vietnam, I left Vietnam when I was 20, so only 20 years, and then Tennessee, and then, so here is the place that I lived the longest, and that gave me a lot of... And besides, this is the capital of the country, and at least, we still the great country in the world, so it\'s very, very honored to live here. I feel very good to be in this area. And with the suburban Virginia and Maryland, I mean, this is very, they call the greater Washington DC, it\'s very dynamic, and trust me, it\'s like a melting pot, a lot of people from different ... In this area, you speak with an accent, nobody pay attention to you, you know? But in some other state, the people say, who is this? Where\'s he come from? But here, it\'s okay.',
+	    location: {
                 center: [-77.08843, 38.96207],
                 zoom: 17.6,
                 pitch: 60,
